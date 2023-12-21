00c482c4a6e9d661cdf47abc92721b403e35b912
IrfanHos029 <houseirfan02@gmail.com>
2023-12-22 00:57:48 +0700

    penambahan program wifi manager selesai,tinggal merapikan dan mengatur tombol restart

--- auto.ino
+++ auto.ino
@@ -2,6 +2,8 @@
 #include <ESP8266mDNS.h>
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
+#include <WiFiManager.h>
+#include <EEPROM.h>
 
 #ifndef STASSID
 #define STASSID "Wifi"
@@ -15,6 +17,7 @@ const char* host = "OTA-LEDS";
 int led_pin = 4;
 #define N_DIMMERS 1
 int dimmer_pin = 16;
+#define pinJum D3
 
 #define sensor D5 //14
 #define led_war D1//5
@@ -28,28 +31,61 @@ bool flag;
 bool stateFlip;
 unsigned long saveTimer = 0;
 unsigned long saveFlip = 0;
+bool wm_nonblocking = false;
+bool stateWifi;
+bool stateMode;
+WiFiManager wifi;
 
+  
 void setup() {
   Serial.begin(115200);
   pinMode(sensor, INPUT_PULLUP);
+  pinMode(pinJum,INPUT_PULLUP);
   pinMode(led_war, OUTPUT);
   pinMode(ledFlip, OUTPUT);
   pinMode(relay_1, OUTPUT);
   digitalWrite(relay_1, LOW);
-
-  /* switch on led */
+  EEPROM.begin(12);
+//  EEPROM.write(0,1); 
+//     EEPROM.commit();
+  if(wm_nonblocking) wifi.setConfigPortalBlocking(false);
+stateWifi = EEPROM.read(0);
+  stateMode = EEPROM.read(0);
+  Serial.println(String()+"stateWifisetup=" + stateWifi + "stateModesetup=" + stateMode);
+  pinMode(dimmer_pin, OUTPUT);
   pinMode(led_pin, OUTPUT);
+  digitalWrite(led_pin, LOW);
+  if(stateWifi==1){
+  wifi.setConfigPortalTimeout(60);
+  bool connectWIFI = wifi.autoConnect("JAM DIGITAL", "00000000");
+  //keluarkan tulisan RTC
+  if (!connectWIFI) {
+    stateWifi=0;
+    Serial.println("NOT CONNECTED TO AP");
+    Serial.println("Pindah ke mode offlien");
+    //showRTC();
+    delay(1000);
+    EEPROM.write(0,stateWifi);
+    EEPROM.commit();
+digitalWrite(led_pin, LOW);
+    ESP.restart();
+  }
+  else
+  {
+    Serial.println("CONNECTED");
+    /* switch on led */
+ // pinMode(led_pin, OUTPUT);
   digitalWrite(led_pin, HIGH);
 
   Serial.println("Booting");
-  WiFi.mode(WIFI_STA);
+ // WiFi.mode(WIFI_STA);
 
-  WiFi.begin(ssid, password);
+ // WiFi.begin(ssid, password);
 
-  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    WiFi.begin(ssid, password);
-    Serial.println("Retrying connection...");
-  }
+//  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+//    WiFi.begin(ssid, password);
+//    Serial.println("Retrying connection...");
+//  }
   /* switch off led */
   digitalWrite(led_pin, LOW);
 
@@ -57,7 +93,7 @@ void setup() {
   analogWriteRange(1000);
   //analogWrite(led_pin, 2);
 
-  pinMode(dimmer_pin, OUTPUT);
+  
   analogWrite(dimmer_pin, 50);
 
 
@@ -83,10 +119,21 @@ void setup() {
   ArduinoOTA.begin();
   Serial.println("Ready");
 
+  delay(1000);
+ // Serial.println(String()+"NTP in the setup:"+ Clock.getHours()+":"+ Clock.getMinutes()+":"+Clock.getSeconds());
+   }
+  }
+  
+
+ //digitalWrite(dimmer_pin, LOW);
+  /* setup the OTA server */
+
+
 }
 
 void loop() {
   ArduinoOTA.handle();
+  if(wm_nonblocking) wifi.process();
   cekLogic();
   runWar();
   indikator();
@@ -98,15 +145,24 @@ void loop() {
   }
   digitalWrite(relay_1, stateOut);
 
-  Serial.println(String() + "stateOut:" + stateOut);
-  // Serial.println(String() + "trigg   :"+ trigg);
-  Serial.println(String() + "Time   :" + Time);
+//  Serial.println(String() + "stateOut:" + stateOut);
+//  // Serial.println(String() + "trigg   :"+ trigg);
+//  Serial.println(String() + "Time   :" + Time);
+if(digitalRead(pinJum)==LOW){  
+  stateWifi = !stateWifi; 
+  delay(2000); 
+  digitalWrite(dimmer_pin, HIGH);
+  digitalWrite(led_pin, HIGH);
+  EEPROM.write(0,stateWifi); 
+     EEPROM.commit();
+  delay(1000);
+  ESP.restart();}
 }
 
 void cekLogic()
 {
   int data = digitalRead(sensor);
-  Serial.println(String() + "data:" + data);
+ // Serial.println(String() + "data:" + data);
   //flag = data;
   if (data == HIGH) {
     stateOut = 1;
